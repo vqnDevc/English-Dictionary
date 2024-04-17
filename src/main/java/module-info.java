1d7a8022b1677c234f45874574512a93c6f06a32
little_boyc7 <91727541+vqnDevc@users.noreply.github.com>
2024-04-17 09:26:29 +0700

    update module project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module org.jmc.dictionary {
     requires de.jensd.fx.glyphs.fontawesome;
     requires java.sql;
     requires javafx.web;
+    requires jlayer;
+    requires freetts;
     requires org.xerial.sqlitejdbc;
 
 
@@ -14,5 +16,8 @@ module org.jmc.dictionary {
     exports org.jmc.dictionary.Controllers.Games;
     exports org.jmc.dictionary.Models;
     exports org.jmc.dictionary.Views;
+    exports org.jmc.dictionary.Management;
+    exports org.jmc.dictionary.Entities;
+    exports org.jmc.dictionary.Utils;
 
 }
